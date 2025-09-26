45b7bf5d6c291060d43dedc8f504662fe39b1603
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-09-26 22:10:53 +0530

    ..

--- Fontend/src/pages/SignUp.jsx
+++ Fontend/src/pages/SignUp.jsx
@@ -2,60 +2,70 @@ import React, { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import logo from "../assets/playtube.jpg";
 import { CgProfile } from "react-icons/cg";
-import {  toast } from 'react-toastify';
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
+import axios from 'axios';
 const SignUp = () => {
   const [page, setpage] = useState(1);
-  const [userName, setuserName] = useState()
-  const [email, setemail] = useState()
-  const [password, setpassword] = useState()
-  const [confirmPassword, setconfirmPassword] = useState()
-  const [showpass, setshowpass] = useState(false)
-  const [fontImage, setfontImage] = useState(null)
-  const [backendImage, setbackendImage] = useState(null)
+  const [userName, setuserName] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [showpass, setshowpass] = useState(false);
+  const [fontImage, setfontImage] = useState(null);
+  const [backendImage, setbackendImage] = useState(null);
+  const navigate = useNavigate();
 
-  const handelar=(e)=>{
-  const file=e.target.files[0]
-  setbackendImage(file);
-  setfontImage(URL.createObjectURL(file))
-  }
+  const handelar = (e) => {
+    const file = e.target.files[0];
+    setbackendImage(file);
+    setfontImage(URL.createObjectURL(file));
+  };
 
- const handelNext=(e)=>{
- if (page>0) {
-   if (!userName) {
-    toast.error("User name required")
-    return
-  }
-   if (!email) {
-    toast.error("Email required")
-    return
-  }
-   if (!password) {
-    toast.error("password required")
-    return
-  }
-   if (!confirmPassword) {
-    toast.error("Confirm Password required")
-    return
-  }
-   if (password!==confirmPassword) {
-    toast.error("Password not match")
-    return
-  }
-  setpage(page+1)
- }
- 
- }
+  const handelNext = () => {
+    if (page === 1) {
+      if (!userName) {
+        toast.error("User name required");
+        return;
+      }
+      if (!email) {
+        toast.error("Email required");
+        return;
+      }
+    }
+    if (page === 2) {
+      if (!password) {
+        toast.error("password required");
+        return;
+      }
+      if (!confirmPassword) {
+        toast.error("Confirm Password required");
+        return;
+      }
+      if (password !== confirmPassword) {
+        toast.error("Password is not match");
+        return;
+      }
+    }
+    setpage(page + 1);
+  };
+
+  const handelBack = () => {
+    if (page > 1) {
+      setpage(page - 1);
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
-    <div
-      className="bg-black h-screen w-screen text-white flex justify-center items-center 
-    "
-    >
+    <div className="bg-black h-screen w-screen text-white flex justify-center items-center">
       <div className="h-auto bg-gray-800 w-auto p-5 rounded-2xl flex flex-col gap-4">
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 cursor-pointer" onClick={handelBack}>
           <FaArrowLeft />
           <p className="text-2xl">Create Account</p>
         </div>
-        {page == 1 && (
+        {page === 1 && (
           <>
             <div className="flex items-center gap-4">
               <img src={logo} alt="" className="h-8 w-8 rounded-4xl" />
@@ -67,7 +77,7 @@ const SignUp = () => {
               placeholder="UserName"
               className="outline w-full p-2 rounded text-white focus:outline-orange-500"
               value={userName}
-              onChange={(e)=>setuserName(e.target.value)}
+              onChange={(e) => setuserName(e.target.value)}
               required
             />
             <input
@@ -75,68 +85,88 @@ const SignUp = () => {
               placeholder="Email"
               className="outline w-full p-2 rounded text-white focus:outline-orange-500"
               value={email}
-              onChange={(e)=>setemail(e.target.value)} 
+              onChange={(e) => setemail(e.target.value)}
               required
             />
-            <button className="bg-orange-400 w-25 p-2 rounded-4xl ml-45 mt-4 cursor-pointer hover:bg-amber-700" onClick={handelNext}>
+            <button
+              className="bg-orange-400 w-25 p-2 rounded-4xl ml-45 mt-4 cursor-pointer hover:bg-amber-700"
+              onClick={handelNext}
+            >
               Next
             </button>
           </>
         )}
-        {page == 2 && (
+        {page === 2 && (
           <>
             <div className="flex items-center gap-4">
               <img src={logo} alt="" className="h-8 w-8 rounded-4xl" />
               <p className="text-2xl font-extrabold">Security</p>
             </div>
-          <div className="flex items-center gap-2 bg-gray-600 p-2 rounded-4xl">
-            <CgProfile className="w-8 h-8"/>
-            <p>{email}</p>
-          </div>
+            <div className="flex items-center gap-2 bg-gray-600 p-2 rounded-4xl">
+              <CgProfile className="w-8 h-8" />
+              <p>{email}</p>
+            </div>
             <input
-              type={showpass?"text":"password"}
+              type={showpass ? "text" : "password"}
               placeholder="Password"
               className="outline w-full p-2 rounded text-white focus:outline-orange-500"
-               value={password}
-              onChange={(e)=>setpassword(e.target.value)} 
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
               required
             />
             <input
-             type={showpass?"text":"password"}
+              type={showpass ? "text" : "password"}
               placeholder="Confirm password"
               className="outline w-full p-2 rounded text-white focus:outline-orange-500"
-               value={confirmPassword}
-              onChange={(e)=>setconfirmPassword(e.target.value)}
+              value={confirmPassword}
+              onChange={(e) => setconfirmPassword(e.target.value)}
               required
             />
-            <div onChange={()=>setshowpass(!showpass)}>
-              <input type="checkbox" name="showpass" id="showpass" required />
-              <label htmlFor="showpass" className="text-gray-100 ml-2 font-bold">Show Password</label>
+            <div>
+              <input
+                type="checkbox"
+                name="showpass"
+                id="showpass"
+                checked={showpass}
+                onChange={() => setshowpass(!showpass)}
+              />
+              <label htmlFor="showpass" className="text-gray-100 ml-2 font-bold">
+                Show Password
+              </label>
             </div>
-            <button className="bg-orange-400 w-25 p-2 rounded-4xl ml-45 mt-4 cursor-pointer hover:bg-amber-700" onClick={handelNext}>
+            <button
+              className="bg-orange-400 w-25 p-2 rounded-4xl ml-45 mt-4 cursor-pointer hover:bg-amber-700"
+              onClick={handelNext}
+            >
               Next
             </button>
           </>
         )}
-         {page == 3 && (
+        {page === 3 && (
           <>
             <div className="flex items-center gap-4">
               <img src={logo} alt="" className="h-8 w-8 rounded-4xl" />
               <p className="text-2xl font-extrabold">Choose Avatar</p>
             </div>
-          <div className="flex  items-center gap-1">
-           <div>
-             {
-              fontImage?<img src={fontImage} alt="profileimage" required />:<CgProfile  className="h-22 w-22"/>
-             }
-           </div>
-           <div className="flex flex-col w-58 p-2 gap-2">
-            <b>Choose Profile Picture</b>
-            <input type="file"  className="bg-orange-400 p-2 rounded-2xl" onChange={handelar}/>
-           </div>
-          </div>
+            <div className="flex  items-center gap-1">
+              <div>
+                {fontImage ? (
+                  <img src={fontImage} alt="profileimage" />
+                ) : (
+                  <CgProfile className="h-22 w-22" />
+                )}
+              </div>
+              <div className="flex flex-col w-58 p-2 gap-2">
+                <b>Choose Profile Picture</b>
+                <input
+                  type="file"
+                  className="bg-orange-400 p-2 rounded-2xl"
+                  onChange={handelar}
+                />
+              </div>
+            </div>
             <button className="bg-green-400 w-40 p-2 rounded-4xl ml-35 mt-4 cursor-pointer hover:bg-amber-700">
-             Create Account
+              Create Account
             </button>
           </>
         )}
